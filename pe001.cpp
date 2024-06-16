60c026bde00b89f8210f74337059a3e3fca0ea1c
ferris <ferrisnantucket@gmail.com>
2024-06-16 17:09:58 -0600

    solved problem 1 233168

---
+++ pe001.cpp
@@ -0,0 +1,25 @@
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6 and 9.
+//The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i=0;
+	//learn to make a stack. I bet it uses pointers.
+	int sum=0;
+	for(i=0;i<999;i++)
+	{
+		if((i+1)%3==0)//15 is added here
+		{
+			sum+=(i+1);
+		}
+		else if((i+1)%5==0)//thus 15 is skipped here and avoids duplicate summation.
+		{
+			sum+=(i+1);
+		}
+	}
+	cout << sum << endl;	
+	
+}
