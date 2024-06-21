979fa396fad1802ede6ed2f0b34d95e96bf73ab8
ferrisnantucket <ag71191@gmail.com>
2024-06-21 13:20:31 -0600

    solved pe005 using recursion to find largest prime factors to multiply

--- pe005.cpp
+++ pe005.cpp
@@ -4,112 +4,61 @@
 #include <iostream>
 #include <cmath>
 
-void getFactors(int num, int (&temp)[8]);
-
-int main()
+//void getFactors(int num, int (&temp)[8]);
+bool isPrime(int num)
 {
-	//std::cout<<5*7*8*9;
-	int arr[8]={2,3,5,7,11,13,17,19};
-	int largest[]={0,0,0,0,0,0,0,0};
-	int temp[8];
-	for(int i=2;i<11;i++)
+	for(int i=sqrt(num);i>0;i--)
 	{
-		for(int x=0;x<8;x++)temp[x]=0;
-		getFactors(i,temp);
-		for(int j=0;j<8;j++)
-		{
-			if(temp[j]>largest[j])largest[j]=temp[j];
-		}
+		if(i==1)return true;
+		else if(num%i==0)return false;
 	}
-	for(int x=0;x<8;x++)std::cout<<largest[x]<<", ";
-	//getFactors(20);
-	return 0;
 }
-void getFactors(int num, int (&temp)[8])
+//void getFactors(int num, int temp[]);
+void getFactors(int num, int temp[])
 {
-	//std::cout << s << std::endl;
-	
+	if(isPrime(num)){temp[num]++;return;}
 	for(int s=num-1;s>0;s--)
 	{
-		if(s==1)
-		{
-			//std::cout<<std::endl;
-			return;
-		}
+		if(s==1)return;
 		else if(num%s==0)
 		{	
-			if(s==2)
-			{
-				//std::cout<<num<<" :"<<s<<std::endl;
-				temp[0]++;
-			}
-			else if(s==3)
-			{
-				temp[1]++;
-			}
-			else if(s==5)
-			{
-				temp[2]++;
-			}
-			else if(s==7)
-			{
-				temp[3]++;
-			}
-			else if(s==11)
-			{
-				temp[4]++;
-			}
-			else if(s==13)
-			{
-				temp[5]++;
-			}
-			else if(s==17)
-			{
-				temp[6]++;
-			}
-			else if(s==19)
-			{
-				temp[7]++;
-			}
-
-			if(num/s==2)
-			{
-				//std::cout<<num<<" :"<<s<<std::endl;
-				temp[0]++;
-			}
-			else if(num/s==3)
-			{
-				temp[1]++;
-			}
-			else if(num/s==5)
-			{
-				temp[2]++;
-			}
-			else if(num/s==7)
-			{
-				temp[3]++;
-			}
-			else if(num/s==11)
-			{
-				temp[4]++;
-			}
-			else if(num/s==13)
-			{
-				temp[5]++;
-			}
-			else if(num/s==17)
-			{
-				temp[6]++;
-			}
-			else if(num/s==19)
-			{
-				temp[7]++;
-			}
-			getFactors(s, temp);
-			getFactors(sqrt(num/s), temp);
+			if(isPrime(s))temp[s]++;
+			else getFactors(s, temp);
+			if(isPrime(num/s))temp[num/s]++;
+			else getFactors((num/s), temp);
+			return;
 		}
 	}
 }
+int main()
+{
+	int arrSize=21;
+	int largest[arrSize];
+	int temp[arrSize];
+	for(int x=0;x<arrSize;x++)largest[x]=temp[x]=0; //reset arr to 0s
+	for(int i=2;i<arrSize+1;i++)
+	{
+		for(int x=0;x<arrSize;x++)temp[x]=0; //reset arr to 0s
+		getFactors(i,temp);
+		for(int j=0;j<arrSize;j++)if(temp[j]>largest[j])largest[j]=temp[j]; //set largest values in largest array
+	}
+	int prod=1;
+	
+	for(int x=1;x<arrSize;x++)
+	{
+		prod=static_cast<int>(round(prod*pow(x,largest[x]))); //pow function returns a float which causes floating point error
+	}
+	
+	
+	for(int x=0;x<arrSize;x++)std::cout<<largest[x]<<", ";
+	std::cout<<std::endl;
+	
+	
+	std::cout<<"Product: "<<prod<<std::endl;
+	
+	return 0;
+}
+
 
 //generate all prime factors used for each number and record the highest number used for each prime.
 //for example, 16 is made up of 2 * 2 * 2 * 2 and has the highest number of 2's used and thus a quantity 4 will be recorded for the factor "2".
