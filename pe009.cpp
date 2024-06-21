70e37c7595dec2ec4f3018d020714d8d28873782
ferrisnantucket <ag71191@gmail.com>
2024-06-21 17:31:34 -0600

    solved pe009

--- pe009.cpp
+++ pe009.cpp
@@ -10,5 +10,22 @@
 
 int main()
 {
+	//brute force
+	//triple for loop and c is always bigger than a and b individually.
+	long long as=0,bs=0,cs=0;
+	for(int a=0;a<1000;a++)
+	{
+		for(int b=0;b<1000;b++)
+		{
+			for(int c=0;c<1000;c++)
+			{
+				as=a*a;
+				bs=b*b;
+				cs=c*c;
+				if(c<a || c<b)continue;
+				else if(a+b+c==1000 && (as+bs)==cs)std::cout<<"A: "<<a<<", A^2: "<<(a*a)<<"B: "<<b<<", B^2: "<<(b*b)<<"C: "<<c<<", C^2: "<<(c*c)<<", ABC: "<<a*b*c<<std::endl;
+			}
+		}
+	}
 	return 0;
 }
